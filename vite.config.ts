b7c0261bf0caed54f8d72978a809a4ef70eda037
mcgonzalez79 <115239584+mcgonzalez79@users.noreply.github.com>
2025-08-11 22:06:41 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // For GitHub Pages only: set base to "/<your-repo-name>/" and uncomment:
-  base: "/launch-tracker/",
+  base: "/launch-tracker/", // 👈 important for GitHub Pages
 });
