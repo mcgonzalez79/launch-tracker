d5280855bc3e3652df1db9ea13ea4ccf08aa60af
mcgonzalez79 <115239584+mcgonzalez79@users.noreply.github.com>
2025-08-21 10:56:49 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'; // <- add this line
 
 const el = document.getElementById('root');
 if (el) {
-  const App = App as React.FC;
+  const RootApp = App as React.FC;
   createRoot(el).render(
     <React.StrictMode>
       <App />
