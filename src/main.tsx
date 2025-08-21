76bff42cc5e743d6ef23cef525e0dd9435ea656d
mcgonzalez79 <115239584+mcgonzalez79@users.noreply.github.com>
2025-08-21 10:55:08 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.css'; // <- add this line
 
 const el = document.getElementById('root');
 if (el) {
